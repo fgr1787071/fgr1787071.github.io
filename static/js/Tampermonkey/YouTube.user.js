7e6eb34e26e86cd336722ac6726048a4fc67f8e0
fgr178707 <fgr178707@163.com>
2024-08-28 20:22:14 +0800

    更新 YouTube.js

--- static/js/Tampermonkey/YouTube.user.js
+++ static/js/Tampermonkey/YouTube.user.js
@@ -1,24 +1,11 @@
 // ==UserScript==
-// @name         115pan_aria2
-// @namespace    115pan_aria2
-// @version      1.1.7
-// @author       f
-// @description  115文件导出到 Aria2
-// @icon         https://115.com/web_icon.jpg
-// @match        *://115.com/?ct=file*
-// @connect      115.com
-// @connect      192.168.50.44
-// @connect      *
-// @grant        GM.xmlHttpRequest
-// @grant        GM.setValue
-// @grant        GM.getValue
-// @grant        unsafeWindow
-// @grant        GM_addValueChangeListener
-// @grant        GM_removeValueChangeListener
-// @grant        GM_getValue
-// @grant        GM_setValue
-// @grant        GM_xmlhttpRequest
-// @run-at       document-end
+// @name         自动跳过YouTube广告
+// @namespace    youtube
+// @version      1.0
+// @description  在YouTube网页上自动跳过广告
+// @author       Joey Gambler
+// @match        *://www.youtube.com/*
+// @grant        none
 // ==/UserScript==
 
 (function () {
@@ -35,8 +22,3 @@
     // 设置检测时间间隔
     var timer = setInterval(skipAd, 1000); // 1000毫秒 = 1秒
 })();
-
-
-
-
-
